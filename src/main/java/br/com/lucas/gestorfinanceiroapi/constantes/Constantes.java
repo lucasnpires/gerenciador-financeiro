0ff35182ca139b5ae655c3a28099df7375020caa
lucasnpires <npires.lucas@gmail.com>
2019-04-19 10:57:36 -0300

    ajuste constantes

--- src/main/java/br/com/lucas/gestorfinanceiroapi/constantes/Constantes.java
+++ src/main/java/br/com/lucas/gestorfinanceiroapi/constantes/Constantes.java
@@ -18,10 +18,6 @@ public class Constantes {
 
      public static final String SOURCE_MIDDLEWARE = "MIDDLEWARE";
 
-     public static final String SOURCE_BLUE = "BLUE API";
-
-     public static final String SOURCE_PIER = "PIER API";
-
      public static final String CONTENT_TYPE = "Content-Type";
 
      public static final String ACCEPT = "Accept";
@@ -30,85 +26,11 @@ public class Constantes {
 
      public static final String CONTENT_TYPE_APPLICATTION_JSON_UTF8 = "application/json;charset=UTF-8";
 
-     public static final String VAR_PIER_URL = "pier_url";
-
-     public static final String VAR_NEUROTECH_POLITICAS_URL = "neurotech_politicas_url";
-
-     public static final String VAR_NEUROTECH_CLIENT_ID = "neurotech_client_id";
-
-     public static final String VAR_NEUROTECH_ACCESS_TOKEN = "neurotech_access_token";
-
-     public static final String VAR_PIER_ACCESS_TOKEN = "pier_access_token";
-
-     public static final String VAR_PIER_CLIENT_ID = "pier_client_id";
-
-     public static final String VAR_PIER_ID_TEMPLATE_EMAIL_RECUPERACAO_SENHAS = "identificacao_template_email_rec_senha";
-
-     public static final String VAR_URL_CADASTRO_SENHA = "url_cadastro_senha";
-
-     public static final String VAR_URL_OAUTH_GERAR_CODIGO = "url_oauth_gerar_codigo";
-
-     public static final String VAR_URL_OAUTH_GERAR_TOKEN = "url_oauth_gerar_token";
-
      public static final String VAR_DATABASE_NAME = "database_name";
 
      public static final String CNPJ = "cnpj";
 
-     private static final String PATH_PIER_NOTIFICACOES_SMS = "/api/notificacoes-sms";
-
-     public static final String PATH_PIER_CODIGOS_SEGURANCA_ENVIAR = PATH_PIER_NOTIFICACOES_SMS + "/gerar-codigo-seguranca";
-
-     public static final String PATH_PIER_CODIGOS_SEGURANCA_REENVIAR = PATH_PIER_NOTIFICACOES_SMS + "/reenviar-codigo-seguranca";
-
-     public static final String PATH_PIER_CODIGOS_SEGURANCA_VALIDAR = PATH_PIER_NOTIFICACOES_SMS + "/validar-codigo-seguranca";
-
-     public static final String PATH_PIER_CNAE = "/api/cnaes";
-
-     public static final String PATH_PIER_BANCO = "/api/bancos";
-
-     public static final String PATH_PIER_GRUPO_ECONOMICO = "/api/grupos-economicos";
-
-     public static final String PATH_PIER_ESTABELECIMENTO = "/api/estabelecimentos";
-
-     public static final String PATH_PIER_TELEFONE_ESTABELECIMENTO = "/api/telefones-estabelecimentos";
-
-     public static final String PATH_PIER_USUARIO = "/api/usuarios";
-
-     public static final String PATH_PIER_ENVIAR_EMAIL = "/api/notificacoes-email";
-     
-     public static final String PARAM_PIER_ACCESS_TOKEN = "access_token";
-
-     public static final String PARAM_PIER_CLIENT_ID = "client_id";
-
-     public static final String PARAM_PIER_CPF = "cpf";
-
-     public static final String PARAM_PIER_CNPJ = "cnpj";
-
-     public static final String PARAM_PIER_ID_PLATAFORMA = "idPlataforma";
-
-     public static final String NEUROTECH_POLITICA = "SANTANDER_P1";
-
-     public static final String FIELD_SERIAL_VERSION_UID = "serialVersionUID";
-
-     public static final String PLATAFORMA_DESCRICAO_CACHE = "PLATAFORMA_DESCRICAO_CACHE";
-
-     public static final String PLATAFORMAS_TOKEN_CACHE = "PLATAFORMAS_TOKEN_CACHE";
-
-     public static final String PATH_CIPHER_DECRYPT = "/decrypt";
-
-     public static final String PATH_CIPHER_GERAR_PIN = "/gerarpin";
-
-     public static final String USUARIO_RESPONSAVEL = "PIER";
-
      public static final Integer CARTOES = 23;
-     
-     public static final String PATH_BLUE_SOCIOS = "/socios";
-     
-     public static final String PATH_BLUE_ESTABELECIMENTOS = "/estabelecimentos";
-     
-     public static final String PATH_INSCRICOES = "/inscricoes";
-     
-     public static final String TIPO_ESTABELECIMENTO_RESTAURANTE = "Restaurante";
      
 
 }
--- src/main/java/br/com/lucas/gestorfinanceiroapi/util/AppConstantes.java
+++ src/main/java/br/com/lucas/gestorfinanceiroapi/util/AppConstantes.java
@@ -3,7 +3,7 @@ package br.com.lucas.gestorfinanceiroapi.util;
 public interface AppConstantes {
 
 	// REST
-	String PATH_DESPESAS = "/despesas";
+	String PATH_DESPESAS = "/despesa";
 	
 	String PATH_CATEGORIAS = "/categoria";
 	
@@ -11,131 +11,14 @@ public interface AppConstantes {
 
 	String PATH_LISTAR = "/listar";
 
-	String PATH_FUNCIONARIOS = "/funcionarios";
-
-	String PATH_GRUPO_EMPRESAS = "/grupos-empresas";
-
-	String PATH_SUBGRUPO_EMPRESAS = "/subgrupos-empresas";
-
-	String PATH_ESTABELECIMENTOS = "/estabelecimentos";
-
-	String PATH_BLACKLIST_PALAVRAS = "/blacklistpalavras";
-
 	String PATH_VALIDAR = "/validar";
 
 	String PATH_ID = "/{id}";
 
-	String PATH_PERMISSOES_USUARIOS_EC = "/ec/permissoes";
-
-	String PATH_PERMISSOES_GRUPO_ESTABELECIMENTOS = "/permissoes-gruposestabelecimentos";
-
-	String PATH_PERMISSOES_ESTABELECIMENTOS = "/permissoes-estabelecimentos";
-
-	String PATH_EMPRESAS = "/empresas";
-
 	String PATH_CARTOES = "/cartoes";
-
-	String PATH_CARTOES_NOTIFICACOES_APN = "/{id}/apnsubscriptions";
-
-	String PATH_NOTIFICACOES_APN = "/apnsubscriptions";
-
-	String PATH_ESTABELECIMENTOS_HORARIOS = "/horarios";
-
-	String PATH_CARTOES_VALIDAR_SENHA = "/{id}/senhas";
-
-	String PATH_FUNCIONARIOS_PRODUTOS = "/funcionariosprodutos";
-
-	String PATH_GRUPOS_ECONOMICOS = "/grupos-economicos";
-
-	String PATH_CARGAS = "/cargas";
-
-	String PATH_ID_CARGAS = "/{id}/cargas";
-
-	String PATH_HISTORICO_PEDIDOS = "/cargas/{idCargaBeneficio}/empresas";
-
-	String PATH_FUNCIONARIOS_PRODUTOS_CARGAS = "/{id}/cargas";
-
-	String PATH_ESTABELECIMENTO_HORARIO = "/estabelecimentos/{id}/horarios";
-
-	String PATH_ESTABELECIMENTO_PRODUTO = "/{id}/produtos";
-
-	String PATH_CONTAS_EXTRATO = "/{id}/extratos";
-
-	String PATH_PARAMETROS_GRUPOS_EMPRESAS = PATH_GRUPO_EMPRESAS + "/{idGrupoEmpresa}/parametros";
-
-	String PATH_PARAMETROS = "/parametros/{codigo}";
-
-	String PATH_PERFIS = "/perfis-acesso";
-
+	
 	String PATH_USUARIOS = "/usuarios";
 
-	String PATH_ESTABELECIMENTOS_TAXAS = "/estabelecimentos/{id}/taxas";
-
-	String PATH_VALIDAR_ESTABELECIMENTOS = "/validar-dados-iniciais";
-
-	String PATH_PIER_NOTIFICACOES_SMS = "/notificacoes-sms";
-
-	String PATH_PIER_CODIGOS_SEGURANCA_ENVIAR = PATH_PIER_NOTIFICACOES_SMS + "/gerar-codigo-seguranca";
-
-	String PATH_PIER_CODIGOS_SEGURANCA_VALIDAR = PATH_PIER_NOTIFICACOES_SMS + "/validar-codigo-seguranca";
-
-	String PATH_PIER_CODIGOS_SEGURANCA_REENVIAR = PATH_PIER_NOTIFICACOES_SMS + "/reenviar-codigo-seguranca";
-
 	String PATH_TOKENS = "/tokens";
-
-	String PATH_SOCIOS = "/socios";
-
-	String PATH_FERIADOS = "/feriados";
-
-	String PATH_PERMISSOES_RH = "/rh/permissoes";
-
-	String PATH_PUSH_NOTIFICATIONS = "/notificacoes";
-
-	String PATH_USUARIOS_OPERACAO = "/operacoes";
-
-	String PATH_PERMISSOES_OPERACAO = "/operacoes";
-
-	String PATH_PERMISSOES_OPERACOES_RH = "/rh/permissoes/{id}/operacoes";
-
-	String PATH_EC_USUARIOS = "/ec/usuarios";
-
-	String PATH_CARTOES_RASTREAMENTO = "/{id}/rastreamentos";
-
-	String PATH_PERMISSOES = "/rh/permissoes";
-
-	String PATH_OPERACOES = "/operacoes";
-
-	String PATH_USUARIOS_ACESSO_RH = "/acessos";
-
-	String PATH_CNAES = "/cnaes";
-
-	// RABBITMQ
-	String AMQP_ECFINDER_E = "blue.ecfinder.estabelecimento.E";
-
-	String AMQP_ECFINDER_Q = "blue.ecfinder.estabelecimento.Q";
-
-	String AMQP_ECFINDER_DLX = "blue.ecfinder.estabelecimento.DLX";
-
-	String AMQP_ECFINDER_DLQ = "blue.ecfinder.estabelecimento.DLQ";
-
-	String EXCHANGE_BLUE_API = "blue.blueapi.exchange";
-
-	String EXCHANGE_ESTABELECIMENTO_CNAE = "blue.blueapi.exchange.estabelecimento";
-
-	String QUEUE_BLUE_SOCIO_USUARIO = "blue.blueapi.cadastro_socio_usuario.QUEUE";
-
-	String QUEUE_BLUE_ESTABELECIMENTO_CNAE = "blue.blueapi.estabelecimento.QUEUE.ecfinder";
-
-	String ROUTING_KEY_NAME_SOCIO_USUARIO = "blue.blueapi.cadastro_socio_usuario*";
-
-	String ROUTING_KEY_NAME_ESTABELECIMENTOS = "blue.blueapi.estabelecimento.QUEUE.*";
-
-	String DEAD_LETTER_SOCIO_USUARIO_QUEUE = "blue.blueapi.cadastro_socio_usuario.dead-letter.queue";
-
-	String DEAD_LETTER_BLUE_ESTABELECIMENTO_QUEUE = "blue.blueapi.estabelecimento.ecfinder.DLQ";
-
-	String DLX_SOCIO_USUARIO_QUEUE = "blue.blueapi.cadastro_socio_usuario.dlx";
-
-	String DLX_ESTABELECIMENTO = "blue.blueapi.estabelecimento.dlx";
 
 }
